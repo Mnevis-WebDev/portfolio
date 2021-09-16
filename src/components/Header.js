44d5c6a90728ce569f9ac1b62f9bc4224220d747
Mnevis <Mnevis.webdev@gmail.com>
2021-09-16 01:44:36 -0400

    add the particules things

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <ul className='header'>
+        <li>
+          <a href='/'>Home</a>
+        </li>
+        <li>
+          <a href='/About'>About</a>
+        </li>
+        <li>
+          <a href='/Resume'>Resume</a>
+        </li>
+        <li>
+          <a href='/Project'>Project</a>
+        </li>
+        <li>
+          <a href='/Contact'>Contact</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header
