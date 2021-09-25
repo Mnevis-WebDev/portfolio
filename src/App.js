6e5179b1fc7fbafda983eb96c65e025e1a775071
Mnevis <Mnevis.webdev@gmail.com>
2021-09-25 06:53:34 -0400

    clean the code with HashRouter in app js instead of index.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Switch from 'react-bootstrap/esm/Switch'
+//import Switch from 'react-bootstrap/esm/Switch'
+import { Switch } from 'react-router'
 import Particles from 'react-particles-js'
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import { BrowserRouter as Router, HashRouter, Route } from 'react-router-dom'
 
 import Header from './components/Header'
 import Home from './screens/Home'
@@ -8,24 +9,26 @@ import Projects from './screens/Projects'
 
 function App() {
   return (
-    <div className='App'>
-      <Particles className='particles' />
-      <div
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-        }}
-      >
-        <Header />
-        <Switch>
-          <Route path='/' exact component={Home}></Route>
-          <Route path='/Projects' component={Projects}></Route>
-        </Switch>
+    <Router>
+      <div className='App'>
+        <Particles className='particles' />
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          <Header />
+          <Switch>
+            <Route path='/' exact component={Home}></Route>
+            <Route path='/Projects' component={Projects}></Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,13 +7,13 @@ const Header = () => {
     <Navbar bg='light' expand='sm'>
       <Container>
         <LinkContainer to='/'>
-          <Navbar.Brand href='#home'>Mnevis's portfolio</Navbar.Brand>
+          <Navbar.Brand>Mnevis's portfolio</Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='m-auto'>
             <LinkContainer to='/'>
-              <Nav.Link href='/'>Home</Nav.Link>
+              <Nav.Link>Home</Nav.Link>
             </LinkContainer>
             <Nav.Link href='#about'>About</Nav.Link>
             <Nav.Link href='#resume'>Resume</Nav.Link>
